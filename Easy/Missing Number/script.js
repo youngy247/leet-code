5453e679544dc063667b75d619ff69c027961648
Adam Young <adamyoungy678@gmail.com>
2023-11-08 14:52:14 +0000

    refactor: improved missing number solution

--- Easy/Missing Number/script.js
+++ Easy/Missing Number/script.js
@@ -3,23 +3,7 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-  nums.sort((a, b) => a - b);
-  console.log(nums);
-  if (nums.length == 1 && nums[0] !== 0) {
-    return 0;
-  }
-
-  if (nums.length == 1 && nums[0] == 0) {
-    return 1;
-  }
-
-  if (nums.length > 1 && nums[0] !== 0) {
-    return 0;
-  }
-
-  for (let i = 0; i <= nums.length - 1; i++) {
-    if (nums[i + 1] !== i + 1) {
-      return nums[i] + 1;
-    }
+  for (let i = 0; i <= nums.length; i++) {
+    if (!nums.includes(i)) return i;
   }
 };
