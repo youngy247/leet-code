04579225980f19a1a822c152a3cf5a32bb78e36d
Adam Young <adamyoungy678@gmail.com>
2023-11-08 14:49:27 +0000

    fix: fixed missing number solution for all test cases

--- Easy/Missing Number/script.js
+++ Easy/Missing Number/script.js
@@ -5,6 +5,17 @@
 var missingNumber = function (nums) {
   nums.sort((a, b) => a - b);
   console.log(nums);
+  if (nums.length == 1 && nums[0] !== 0) {
+    return 0;
+  }
+
+  if (nums.length == 1 && nums[0] == 0) {
+    return 1;
+  }
+
+  if (nums.length > 1 && nums[0] !== 0) {
+    return 0;
+  }
 
   for (let i = 0; i <= nums.length - 1; i++) {
     if (nums[i + 1] !== i + 1) {
@@ -12,5 +23,3 @@ var missingNumber = function (nums) {
     }
   }
 };
-
-console.log(missingNumber([3, 0, 1]));
