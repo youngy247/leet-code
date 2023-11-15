a977e3ddc7630be902b66b02c08e53c2b887ed9f
Adam Young <adamyoungy678@gmail.com>
2023-11-15 15:53:46 +0000

    refactor solution

--- Easy/Two Sum/script.js
+++ Easy/Two Sum/script.js
@@ -4,13 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = 0; j < nums.length - 1; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+  let numMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
     }
+
+    numMap.set(nums[i], i);
   }
+
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //   for (let j = 0; j < nums.length - 1; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
