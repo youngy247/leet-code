f6b2cb09493cdac4e782ca7a6c0331062f12900b
Adam Young <adamyoungy678@gmail.com>
2023-11-18 12:47:19 +0000

    improved time complexity

--- Easy/Search Insert Position/script.js
+++ Easy/Search Insert Position/script.js
@@ -3,16 +3,25 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return nums.indexOf(nums[i]);
-    } else if (nums[i] > target) {
-      return nums.indexOf(nums[i]);
+var searchInsert = function (arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
     }
   }
 
-  return nums.length;
+  return leftIndex;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 5));
