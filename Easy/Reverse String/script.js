dae39feedeab3cfb2cf2b8a832afd6c137b732f3
Adam Young <adamyoungy678@gmail.com>
2023-11-18 14:03:49 +0000

    fixed solution

--- Easy/Reverse String/script.js
+++ Easy/Reverse String/script.js
@@ -3,5 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-  return s.reverse().join("");
+  return s.split('').reverse().join("");
 };
+
+console.log(reverseString('yes'));
