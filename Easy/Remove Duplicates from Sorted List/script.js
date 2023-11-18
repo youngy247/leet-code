a5bc64d7fe3ca61d6688c0ce03ee297bdafa17b7
Adam Young <adamyoungy678@gmail.com>
2023-11-18 14:10:34 +0000

    updated solution

---
+++ Easy/Remove Duplicates from Sorted List/script.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  const set = new Set(head)
+  const res = [...set]
+  return res
+};
+
+console.log(deleteDuplicates([1,1,2]));
